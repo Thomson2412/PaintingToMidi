1e654f0bde5dd8e11fb2eb7b97975398e6d42831
Thomson2412 <thomasm.w.fink@gmail.com>
2021-01-08 00:54:55 +0100

    Add midi sync

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import cv2
 import mido
 import time
+import threading
 import numpy as np
 
 
 def main():
-    #40
-    #img = cv2.imread("Van Gogh/Saint Remy/Half Figure of an Angel after Rembrandt.jpg")
-    #img = cv2.imread("Van Gogh/Saint Remy/Irises.jpg")
-    #img = cv2.imread("Van Gogh/Paris/Vase with Asters and Phlox.jpg")
-    #80
-    #img = cv2.imread("Van Gogh/Paris/The Kingfisher.jpg")
-    img = cv2.imread("Van Gogh/Paris/The Hill of Montmartre with Quarry.jpg")
+    # img = cv2.imread("Van Gogh/Saint Remy/Half Figure of an Angel after Rembrandt.jpg")
+    img = cv2.imread("Van Gogh/Saint Remy/Irises.jpg")
+    # img = cv2.imread("Van Gogh/Paris/Vase with Asters and Phlox.jpg")
+    # img = cv2.imread("Van Gogh/Paris/The Kingfisher.jpg")
+    # img = cv2.imread("Van Gogh/Paris/The Hill of Montmartre with Quarry.jpg")
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     scale_percent = 25
@@ -22,40 +21,67 @@ def main():
     img = cv2.resize(img, dim)
     hsv_img = cv2.resize(hsv_img, dim)
 
-    #cv2.imshow("OG", img)
-    #cv2.imshow("HSV", hsv_img)
+    # cv2.imshow("OG", img)
+    # cv2.imshow("HSV", hsv_img)
 
+    in_port = mido.open_input("loopMIDI Port 1")
     out_port = mido.open_output("loopMIDI Port 2")
 
     step_size = 80
-    for y in range(0, height, step_size):
-        for x in range(0, width, step_size):
-            sub_img = hsv_img[y:y+step_size, x:x+step_size]
+    x_list = range(0, width, step_size)
+    y_list = range(0, height, step_size)
+    x_index = 0
+    y_index = 0
+
+    clock_count = 0
+    for msg in in_port:
+        if clock_count == 192 or msg.type == "start":
+            x = x_list[x_index]
+            y = y_list[y_index]
+
+            sub_img = hsv_img[y:y + step_size, x:x + step_size]
             mean_hsv = np.round(np.mean(sub_img.reshape(-1, 3), axis=0)).astype(np.int)
 
-            hsv_img[y:y+step_size, x:x+step_size] = img[y:y+step_size, x:x+step_size]
+            hue = scale_between_range(mean_hsv[0], 0, 179, 1, 127)
+            saturation = scale_between_range(mean_hsv[1], 0, 255, 1, 127)
+            intensity = scale_between_range(mean_hsv[2], 0, 255, 1, 127)
+
+            note_duration = scale_between_range(hue + saturation + intensity, 3, 127 *3, 2, 6)
+            make_sound(hue, saturation, intensity, intensity, note_duration, out_port)
+
+            hsv_img[y:y + step_size, x:x + step_size] = img[y:y + step_size, x:x + step_size]
             cv2.imshow("HSV", hsv_img)
             cv2.waitKey(1)
 
-            hue = scale_between_range(mean_hsv[0], 0, 179, 0, 127)
-            saturation = scale_between_range(mean_hsv[1], 0, 255, 0, 127)
-            intensity = scale_between_range(mean_hsv[2], 0, 255, 0, 127)
+            x_index += 1
+            if x_index == len(x_list):
+                x_index = 0
+                y_index += 1
+                if y_index == len(y_list):
+                    break
 
-            hue_note = mido.Message("note_on", note=hue, velocity=intensity)
-            sat_note = mido.Message("note_on", note=saturation, velocity=intensity)
-            int_note = mido.Message("note_on", note=intensity, velocity=intensity)
-            out_port.send(hue_note)
-            out_port.send(sat_note)
-            out_port.send(int_note)
-            time.sleep(scale_between_range(hue + saturation + intensity, 0, 127*3, 1, 5))
-            hue_note = mido.Message("note_off", note=hue, velocity=intensity)
-            sat_note = mido.Message("note_off", note=saturation, velocity=intensity)
-            int_note = mido.Message("note_off", note=intensity, velocity=intensity)
-            out_port.send(hue_note)
-            out_port.send(sat_note)
-            out_port.send(int_note)
+            clock_count = 0
+
+        if msg.type == "clock":
+            clock_count += 1
 
     cv2.destroyAllWindows()
+
+
+def make_sound(note_0, note_1, note_2, vel, dur, out_port):
+    hue_note = mido.Message("note_on", note=note_0, velocity=vel)
+    sat_note = mido.Message("note_on", note=note_1, velocity=vel)
+    int_note = mido.Message("note_on", note=note_2, velocity=vel)
+    out_port.send(hue_note)
+    out_port.send(sat_note)
+    out_port.send(int_note)
+
+    hue_note = mido.Message("note_off", note=note_0, velocity=vel)
+    sat_note = mido.Message("note_off", note=note_1, velocity=vel)
+    int_note = mido.Message("note_off", note=note_2, velocity=vel)
+    threading.Timer(dur, lambda: out_port.send(hue_note)).start()
+    threading.Timer(dur, lambda: out_port.send(sat_note)).start()
+    threading.Timer(dur, lambda: out_port.send(int_note)).start()
 
 
 def scale_between_range(value, in_min, in_max, out_min, out_max):
