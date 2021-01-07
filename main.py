0e4a7584a85fc8197e124b1cebaa30e3ab98b2ce
Thomson2412 <thomasm.w.fink@gmail.com>
2021-01-07 16:01:59 +0100

    Update for windows

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ import numpy as np
 
 
 def main():
-    img = cv2.imread("Van Gogh/Arles/A Lane near Arles.jpg")
+    #40
+    #img = cv2.imread("Van Gogh/Saint Remy/Half Figure of an Angel after Rembrandt.jpg")
+    #img = cv2.imread("Van Gogh/Saint Remy/Irises.jpg")
+    #img = cv2.imread("Van Gogh/Paris/Vase with Asters and Phlox.jpg")
+    #80
+    #img = cv2.imread("Van Gogh/Paris/The Kingfisher.jpg")
+    img = cv2.imread("Van Gogh/Paris/The Hill of Montmartre with Quarry.jpg")
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     scale_percent = 25
@@ -16,16 +22,20 @@ def main():
     img = cv2.resize(img, dim)
     hsv_img = cv2.resize(hsv_img, dim)
 
-    # cv2.imshow("OG", img)
-    cv2.imshow("HSV", hsv_img)
+    #cv2.imshow("OG", img)
+    #cv2.imshow("HSV", hsv_img)
 
-    out_port = mido.open_output()
+    out_port = mido.open_output("loopMIDI Port 2")
 
-    step_size = 40
+    step_size = 80
     for y in range(0, height, step_size):
         for x in range(0, width, step_size):
             sub_img = hsv_img[y:y+step_size, x:x+step_size]
             mean_hsv = np.round(np.mean(sub_img.reshape(-1, 3), axis=0)).astype(np.int)
+
+            hsv_img[y:y+step_size, x:x+step_size] = img[y:y+step_size, x:x+step_size]
+            cv2.imshow("HSV", hsv_img)
+            cv2.waitKey(1)
 
             hue = scale_between_range(mean_hsv[0], 0, 179, 0, 127)
             saturation = scale_between_range(mean_hsv[1], 0, 255, 0, 127)
@@ -33,20 +43,17 @@ def main():
 
             hue_note = mido.Message("note_on", note=hue, velocity=intensity)
             sat_note = mido.Message("note_on", note=saturation, velocity=intensity)
+            int_note = mido.Message("note_on", note=intensity, velocity=intensity)
             out_port.send(hue_note)
             out_port.send(sat_note)
-            print(hue_note)
-            print(sat_note)
-            time.sleep(1)
+            out_port.send(int_note)
+            time.sleep(scale_between_range(hue + saturation + intensity, 0, 127*3, 1, 5))
             hue_note = mido.Message("note_off", note=hue, velocity=intensity)
             sat_note = mido.Message("note_off", note=saturation, velocity=intensity)
+            int_note = mido.Message("note_off", note=intensity, velocity=intensity)
             out_port.send(hue_note)
             out_port.send(sat_note)
-            print(hue_note)
-            print(sat_note)
-            hsv_img[y:y+step_size, x:x+step_size] = img[y:y+step_size, x:x+step_size]
-            cv2.imshow("HSV", hsv_img)
-            cv2.waitKey(1)
+            out_port.send(int_note)
 
     cv2.destroyAllWindows()
 
