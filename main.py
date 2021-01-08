52c0b7a3bc585d362c012712a2e9710b299f5cd5
Thomson2412 <thomasm.w.fink@gmail.com>
2021-01-08 15:17:39 +0100

    Update

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import numpy as np
 
 
 def main():
-    # img = cv2.imread("Van Gogh/Saint Remy/Half Figure of an Angel after Rembrandt.jpg")
-    img = cv2.imread("Van Gogh/Saint Remy/Irises.jpg")
+    img = cv2.imread("Van Gogh/Saint Remy/Half Figure of an Angel after Rembrandt.jpg")
+    # img = cv2.imread("Van Gogh/Saint Remy/Irises.jpg")
     # img = cv2.imread("Van Gogh/Paris/Vase with Asters and Phlox.jpg")
     # img = cv2.imread("Van Gogh/Paris/The Kingfisher.jpg")
     # img = cv2.imread("Van Gogh/Paris/The Hill of Montmartre with Quarry.jpg")
@@ -24,8 +24,8 @@ def main():
     # cv2.imshow("OG", img)
     # cv2.imshow("HSV", hsv_img)
 
-    in_port = mido.open_input("loopMIDI Port 1")
-    out_port = mido.open_output("loopMIDI Port 2")
+    in_port = mido.open_input("loopMIDI Port 0")
+    out_port = mido.open_output("loopMIDI Port 1")
 
     step_size = 80
     x_list = range(0, width, step_size)
@@ -42,11 +42,11 @@ def main():
             sub_img = hsv_img[y:y + step_size, x:x + step_size]
             mean_hsv = np.round(np.mean(sub_img.reshape(-1, 3), axis=0)).astype(np.int)
 
-            hue = scale_between_range(mean_hsv[0], 0, 179, 1, 127)
-            saturation = scale_between_range(mean_hsv[1], 0, 255, 1, 127)
-            intensity = scale_between_range(mean_hsv[2], 0, 255, 1, 127)
+            hue = scale_between_range(mean_hsv[0], 0, 179, 10, 100)
+            saturation = scale_between_range(mean_hsv[1], 0, 255, 10, 100)
+            intensity = scale_between_range(mean_hsv[2], 0, 255, 10, 100)
 
-            note_duration = scale_between_range(hue + saturation + intensity, 3, 127 *3, 2, 6)
+            note_duration = scale_between_range(hue + saturation + intensity, 3, 127 * 3, 2, 6)
             make_sound(hue, saturation, intensity, intensity, note_duration, out_port)
 
             hsv_img[y:y + step_size, x:x + step_size] = img[y:y + step_size, x:x + step_size]
